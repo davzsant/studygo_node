8016f6ffc20d141c0784c50b27d4a20438ec33d6
Millexs <david.lucas.santiago02@gmail.com>
2025-02-10 00:23:04 -0300

    fix: Melhroando o cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import router from './routes'   
 import 'dotenv/config';
-import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 console.log(process.env.DB_USER)
 console.log(process.env.DB_HOST)
